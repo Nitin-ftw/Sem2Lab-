005a4f1b489fccd14ac878fdbb18a5703d9dbac4
Nitin-ftw <nitinedward17@gmail.com>
2026-04-05 23:00:29 +0530

    Refactor math functions and improve precision
    Refactor factorial function and update sine and cosine calculations to use double precision. Add input prompts for x and number of terms.

--- Lab 3.c
+++ Lab 3.c
@@ -1,64 +1,64 @@
-#include <stdio.h>
-#include <math.h>
-int fact(int n);
-float sinx(float x);
-float cosx(float x);
-float series(int n);
+#include<stdio.h>
+#include<math.h>
+
+long int fact(int n)
+{
+    int i;
+    long int f=1;
+    for(i=1;i<=n;i++)
+        f=f*i;
+    return f;
+}
+
 int main()
 {
-    float x ,n;
-    printf("Enter value of x: ");
-    scanf("%f",&x);
-    printf("sin(%f) = %f\n", x, sinx(x));
-    printf("cos(%f) = %f\n", x, cosx(x));
-    printf("Enter value of n: ");
-    scanf("%f",&n);
-    printf("SUM = %f\n", series(n));
+    double x,term,sum=0;
+    int n=1,sign=1;
+
+    printf("Enter x in radians: ");
+    scanf("%lf",&x);
+    
+    while(1)
+    {
+        term=pow(x,n)/(double)fact(n);
+        if (fabs(term)<0.0000001)
+            break;
+        sum+=sign*term;
+        sign=-sign;
+        n+=2;
+    }
+
+    printf("sin(x) = %lf",sum);
+
+    double term2,sum2=1;
+    n=2;
+    sign=-1;
+
+    printf("\nEnter x in radians: ");
+    scanf("%lf",&x);
+
+    while(1)
+    {
+        term2=pow(x,n)/(double)fact(n);
+        if (fabs(term2)<0.0000001)
+            break;
+        sum2+=sign*term2;
+        sign=-sign;
+        n+=2;
+    }
+
+    printf("cos(x) = %lf",sum2);
+    
+    sum=1.0;
+    int i;
+    printf("\nEnter number of terms: ");
+    scanf("%d",&n);
+    
+    for(i=2;i<=n;i++)
+    {
+        sum = sum + pow((1.0/i),i);
+    }
+
+    printf("Sum = %f",sum);
     return 0;
-}
-int fact(int n)
-{
-    if(n == 0 || n == 1)
-        return 1;
-    else
-        return n * fact(n - 1);
-}
-
-float sinx(float x)
-{
-    float term, sum=0;
-    int n = 1,sign = 1;
-    while(1)
-    {
-        term=pow(x,n)/fact(n);
-        if (term<0.0000001)
-            break;
-        sum += sign * term;
-        sign = -sign;
-        n+=2;
-    }
-    return sum;
-}
-
-float cosx(float x)
-{
-    float term, sum=1;
-    int n = 2,sign = 1;
-    while(1)
-    {
-        term=pow(x,n)/fact(n);
-        if (term<0.000001)
-            break;
-        sum += sign * term;
-        sign = -sign;
-        n+=2;
-    }
-    return sum;
-}
-float series(int n)
-{
-    float sum=0;
-    for(int i=1;i<=n;i++)
-        sum+=pow(1.0/i,i);
-    return sum;
 }
